714e65db66157d63b2b7caa1ccebce5ad07f1f8a
Gab-loren7 <gabrielcarvalho09012008@gmail.com>
2025-05-19 16:15:06 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// -> Puxando os elementos em variaveis
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+// -> Cria uma função para método toggle(Se a classe está ela é removida, se não está ela é criada)
+const toggleModal = () => {
+    modal.classList.toggle("hide");
+    fade.classList.toggle("hide");
+};
+
+// -> Faz o efeito de click nessas classes para abrir e fechar
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
